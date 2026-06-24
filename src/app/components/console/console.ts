da5e7e52bc08798eb00dac55748f2d03d8df1aee
puhinetsn82 <puhinetsn82@gmail.com>
2026-06-24 16:01:40 +0300

    comsole and controls layout created

---
+++ src/app/components/console/console.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-console',
+  imports: [],
+  templateUrl: './console.html',
+  styleUrl: './console.scss',
+})
+export class Console {}
---
+++ src/app/components/controls/controls.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-controls',
+  imports: [MatIconModule],
+  templateUrl: './controls.html',
+  styleUrl: './controls.scss',
+})
+export class Controls {}
